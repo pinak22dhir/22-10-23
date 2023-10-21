3feebbac16b01688b09b9d29135ca8063df553d9
pinak dhir <pinakdhir@gmail.com>
2023-10-21 10:55:58 +0530

    modified

--- structures.c
+++ structures.c
@@ -1,1 +1,10 @@
 // for storing different values  int char we use structures
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[]="pinak dhir";
+    char* p=str;
+    *p='P';
+    printf("%s",str);
+    return 0;
+}
